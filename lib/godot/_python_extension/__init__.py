448922d22c7b40ba4b9fdeead570e6e9808d156a
Mai Lavelle <mai.lavelle@gmail.com>
2023-10-09 03:26:02 -0400

    Add PythonScript icon

--- lib/godot/_python_extension/__init__.py
+++ lib/godot/_python_extension/__init__.py
@@ -30,6 +30,28 @@ godot_fs_importer.install()
 cli.main()
 
 
+def _install_icons():
+	# XXX: probably not the best way to do things, but works for now
+
+	if not godot.Engine.is_editor_hint():
+		return
+
+	import importlib.resources
+	package = importlib.resources.files(__package__)
+
+	svg = (package / 'icons' / 'python-script.svg').read_text()
+
+	img = godot.Image()
+	img.load_svg_from_string(svg, godot.EditorInterface.get_editor_scale())
+
+	texture = godot.ImageTexture.create_from_image(img)
+
+	godot.EditorInterface.get_editor_theme().set_icon('PythonScript', 'EditorIcons', texture)
+
+
+godot.Callable(_install_icons).call_deferred()
+
+
 @atexit.register
 def _cleanup():
 	global script_language
---
+++ lib/godot/_python_extension/icons/readme.md
@@ -0,0 +1,2 @@
+The `python-script.svg` icon in this directory is based of off Godot's `editor/icons/TextFile.svg` icon, see `license.md` in this directory for the original license.
+
